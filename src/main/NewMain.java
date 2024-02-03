53e522480542148a1a47896dc7c1f39634e99257
FelipeGo18 <gonzalezgarzon14@hotmail.com>
2024-02-03 11:48:01 -0500

    Creé el menú

--- src/main/NewMain.java
+++ src/main/NewMain.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package main;
 
 import java.util.Scanner;
@@ -16,23 +13,28 @@ public class NewMain {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int a;
-        int b;
-        int c;
         
-        System.out.print("Ingrese un numero");
-        a = sc.nextInt();
-        
-        System.out.print("Ingrese un numero");
-        b = sc.nextInt();
-        
-        c = a + b;
-        
-        System.out.println(c+a);
-        System.out.println(c+1);
-        
-        System.out.println("HOLA MUNDO");
+        System.out.println("""
+                           1.  Crear arreglo
+                           2.  Agrega un elemento al final de la lista
+                           3.  Agrega un elemento en una posicion de la lista
+                           4.  addAll
+                           5.  Limpiar lista
+                           6.  Verificar si hay un elemento en la lista
+                           7.  Mostrar que elemento hay en una posicion
+                           8.  Mostrar la posicion de la primera aparicion del elemento 
+                               especificado en esta lista, o -1 si esta lista no contiene el elemento.
+                           9.  Verificar si la lista está vacia
+                           10. Mostrar la posicion de la ultima aparicion del elemento 
+                               especificado en esta lista, o -1 si esta lista no contiene el elemento.
+                           11. Eliminar un elemento en una posición de la lista
+                           12. Eliminar la priemara aparicion de un elemento de la lista
+                           13. Reemplazar un elemento de la lista
+                           14. Numero de elemetos en la lista
+                           15. Hacer una sublista
+                           16. Disminuir tamaño de la lista
+                           17. Aumenta el tamaño de la lista
+                           """);
     }
     
 }
